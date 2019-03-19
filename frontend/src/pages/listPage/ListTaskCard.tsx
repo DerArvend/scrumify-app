4f1fc020a5df6ccee8cfefeb496ddcd443ab854f
Sergey Kuzminov <derarvend@gmail.com>
2019-03-19 21:46:13 +0500

    frontend: some markup

--- frontend/src/pages/listPage/ListTaskCard.tsx
+++ frontend/src/pages/listPage/ListTaskCard.tsx
@@ -26,16 +26,26 @@ const Card = styled<any>(AntCard)`
     }
 `;
 
+const YouTrackLink = styled.a`
+    word-break: break-all;
+`;
+
+const urlRegex = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+
 export class ListTaskCard extends React.PureComponent<ListTaskCardProps> {
     render() {
         const title = <div style={{ whiteSpace: "normal" }}>{this.props.theme}</div>;
         return <Card title={title}>
-            <Title>Ссылка на YouTrack</Title>
-            <Text><a target="_blank" href={this.props.url}>{this.props.url}</a></Text>
-            <Title>Статус</Title>
-            <Text>{this.props.currentState}</Text>
-            <Title>Проблемы</Title>
-            <Text>{this.props.problems}</Text>
+            {this.props.url && <Title>Ссылка на YouTrack</Title>}
+            {this.props.url && this.renderUrl(this.props.url)}
+            {this.props.currentState && <Title>Статус</Title>}
+            {this.props.currentState && <Text>{this.props.currentState}</Text>}
+            {this.props.problems && <Title>Проблемы</Title>}
+            {this.props.problems && <Text>{this.props.problems}</Text>}
         </Card>;
     }
+
+    private renderUrl = (url: string) => urlRegex.test(url)
+        ? <YouTrackLink target="_blank" href={url}>{url}</YouTrackLink>
+        : <Text>{this.props.url}</Text>
 }
