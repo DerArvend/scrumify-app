0a7f6dbb1620eced82f128e951b9ad7b0dbc229b
Sergey Kuzminov <derarvend@gmail.com>
2019-04-02 03:35:54 +0500

    backend: read sensetive mssql settings from env variables instead of settings.json

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,13 @@ import settings from './settings.json';
 import { MsSqlDataRepository } from './dataAccess/mssql/MsSqlDataRepository.js';
 import { ApiHandler } from './api/ApiHandler';
 
-const repository = new MsSqlDataRepository(settings.mssqlConfig);
+const mssqlConfig = {
+    ...settings.mssqlConfig,
+    user: process.env.SCRUMIFY_DB_USER,
+    password: process.env.SCRUMIFY_DB_PASSWORD,
+    server: process.env.SCRUMIFY_DB_URL,
+};
+
+const repository = new MsSqlDataRepository(mssqlConfig);
 const apiHandler = new ApiHandler(settings, repository);
 apiHandler.startListening();
