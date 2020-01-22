cc3dc8ee1835fc0770c599e3c61267a4a9708e60
Sergey Kuzminov <s.kuzminov@skbkontur.ru>
2020-01-23 01:56:51 +0500

    backend: move to ts-node-dev

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,19 @@
 import settings from './settings.json';
-import { MsSqlDataRepository } from './dataAccess/mssql/MsSqlDataRepository.js';
+import { MsSqlDataRepository } from './dataAccess/mssql/MsSqlDataRepository';
 import { ApiHandler } from './api/ApiHandler';
 
-const mssqlConfig = {
+let mssqlConfig = {
     ...settings.mssqlConfig,
-    user: process.env.SCRUMIFY_DB_USER,
-    password: process.env.SCRUMIFY_DB_PASSWORD,
-    server: process.env.SCRUMIFY_DB_URL,
 };
+
+if (process.env.NODE_ENV === 'production') {
+    mssqlConfig = {
+        ...mssqlConfig,
+        user: process.env.SCRUMIFY_DB_USER,
+        password: process.env.SCRUMIFY_DB_PASSWORD,
+        server: process.env.SCRUMIFY_DB_URL,
+    }
+}
 
 const repository = new MsSqlDataRepository(mssqlConfig);
 const apiHandler = new ApiHandler(settings, repository);
